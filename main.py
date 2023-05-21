15beeeaa92ffaeb644108346107d10dbfaa50453
evgeniia lemanova <jane87.05@mail.ru>
2023-05-21 13:46:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = 'localhost'
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_POST(self):
+        """ Метод для отправки POST-запросов """
+        self._set_headers()
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        print(post_body.decode())
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        self._set_headers()
+        self.wfile.write(bytes("Hello, World wide web!", "utf-8")) # Тело ответа
+
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
+
